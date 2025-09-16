29472a1318e7a7dcdefdb0d1c15ea4dd89f5fb5e
Cho Kisung <kisung128@gmail.com>
2025-09-16 10:50:53 +0900

    사각 운동, 원 운동 뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -3,7 +3,18 @@ import math
 
 open_canvas()
 
+
+def move_rectangle():
+    pass
+
+
+def move_circle():
+    pass
+
+
 while True:
+    move_rectangle()
+    move_circle()
     pass
 
 close_canvas()
